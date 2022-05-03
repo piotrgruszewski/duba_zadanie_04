6043c802d52012bba70eada2f85ede3e727ba9a9
Piotr Gruszewski <piotr.gruszewski@gmail.com>
2022-05-03 18:15:47 +0200

    03

--- Core/Src/gpio.c
+++ Core/Src/gpio.c
@@ -54,7 +54,7 @@ void MX_GPIO_Init(void)
 
   /*Configure GPIO pin : PtPin */
   GPIO_InitStruct.Pin = BB_Pin;
-  GPIO_InitStruct.Mode = GPIO_MODE_IT_FALLING;
+  GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   HAL_GPIO_Init(BB_GPIO_Port, &GPIO_InitStruct);
 
